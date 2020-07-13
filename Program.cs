c56484a201b63ae6c95d001fba059d47b84f44ab
Erich Richter <erich.richter@gmail.com>
2020-07-13 14:45:44 -0700

    add ul and bl > > Co-authored-by: Kate Skorija <kateskorija@gmail.com> Co-authored-by: Noel Kirkland <noelkirkland@gmail.com>

--- Program.cs
+++ Program.cs
@@ -1,12 +1,42 @@
 using System;
 using System.Collections.Generic;
 
-namespace Program
+namespace Game
 {
+  public class Program
+  {
+    public static void Main()
+    {
+      Console.WriteLine("enter a number between 0-100!");
+      string userString = Console.ReadLine();
+      int userInt = int.Parse(userString);
+    
+      List<string> numberList = new List<string>{};
 
+      for (int index = 1; index <= userInt; index ++)
+      {
+        if (index % 15 == 0)
+        {
+          numberList.Add("ping-pong");
+        }
+        else if (index % 3 == 0)
+        {
+          numberList.Add("ping");
+        }
+        else if (index % 5 == 0)
+        {
+          numberList.Add("pong");
+        }
+        else 
+        {
+          numberList.Add(index.ToString());
+        }
+      }
 
-
-
-
-
+      Console.WriteLine(numberList);
+    
+    }
+  }
 }
+
+
